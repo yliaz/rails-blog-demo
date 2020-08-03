867cb06f715208287eac924b294695ca51ec9e04
BarryZhu <zhuye256@163.com>
2020-08-03 17:28:57 +0800

    完成update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
@@ -20,6 +22,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
